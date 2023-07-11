07a56f05493872e2a27bd02f86972a68713cd1e0
AlexGoIT <alexandrpuryaev@gmail.com>
2023-07-11 20:40:28 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,27 +2,6 @@ import PropTypes from 'prop-types';
 import { Component } from 'react';
 import { FilterLabel, FilterInput } from './Filter.styled';
 
-// const Filter = ({ onFilter }) => {
-//   return (
-//     <FilterLabel>
-//       Find contacts by name
-//       <FilterInput
-//         type="text"
-//         placeholder="Input name..."
-//         onChange={e => {
-//           onFilter(e);
-//         }}
-//       />
-//     </FilterLabel>
-//   );
-// };
-
-// export default Filter;
-
-// Filter.propTypes = {
-//   onFilter: PropTypes.func,
-// };
-
 export default class Filter extends Component {
   static propTypes = {
     onFilter: PropTypes.func,
